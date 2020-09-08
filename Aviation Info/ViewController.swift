75701d07fba730f555b0b481609e1814a66b3c53
Henry Jiang <henryjiang97@gmail.com>
2020-09-07 20:50:17 -0700

    Initial Commit

---
+++ Aviation Info/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aviation Info
+//
+//  Created by Henry Jiang on 9/7/20.
+//  Copyright © 2020 Henry Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
